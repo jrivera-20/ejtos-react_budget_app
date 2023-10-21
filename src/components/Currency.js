366b0e4fc55c4dfb4820cc4a04e3a781a2e26bee
Jean <jrivera-20>
2023-10-20 21:00:59 -0400

    complete

--- src/components/Currency.js
+++ src/components/Currency.js
@@ -1,40 +1,37 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import { AppContext } from '../context/AppContext';
-
+import './Currency.css';
 
 const Currency = (props) => {
-  const [ currency,setCurrency ] = useState('');
-  const { dispatch, } = useContext(AppContext);
-  
+  const [currency, setCurrency] = useState('');
+  const { dispatch, currency: currentCurrency } = useContext(AppContext);
+
+  // Set the initial currency value from the context when the component mounts
+  useEffect(() => {
+    setCurrency(currentCurrency);
+  }, [currentCurrency]);
+
   const updateCurrency = (event) => {
-    /*console.log(currency+"....A..."+event.target.value);
-    */
+    const selectedCurrency = event.target.value;
     dispatch({
-          type: 'CHG_CURRENCY',
-          payload: event.target.value,
-        });
-    setCurrency(event.target.value);
-    return;
-  }
+      type: 'CHG_CURRENCY',
+      payload: selectedCurrency,
+    });
+    setCurrency(selectedCurrency);
+  };
 
   return (
-    <div className='alert alert-secondary'>  
-    <span>
-      <select 
-        className='btn btn-success dropdown-toggle'
-        id='currency'
-        onChange={(event) =>updateCurrency(event)}>Currency ({currency})
-          <option defaultValue={currency}>Currency ({currency})</option>
-          <option value='£'>£ Pound</option>
-          <option value='$'>$ Dollar</option>
-          <option value='€'>€ Euro</option>
-          <option value='₹'>₹ Rupee</option>
-      </select>
-      </span>
+    <div className='alert alert-success' style={{ backgroundColor: '#90EE90', padding: '10px' }}>
+      <label style={{ marginLeft: '1rem', backgroundColor: '#90EE90', color: 'white' }}>
+        Currency
+        <select name='hover_color' id="currency" onChange={(event) => updateCurrency(event)} value={currency} style={{ backgroundColor: '#90EE90', color: 'white' }}>
+          <option style={{ color: 'black' }} value="£">£ Pound</option>
+          <option style={{ color: 'black' }} value="$">$ Dollar</option>
+          <option style={{ color: 'black' }} value="€">€ Euro</option>
+          <option style={{ color: 'black' }} value="₹">₹ Rupee</option>
+        </select>
+      </label>
     </div>
-
-
-
   );
 };
 
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -5,7 +5,7 @@ import { AiFillMinusCircle } from 'react-icons/ai'
 import { AppContext } from '../context/AppContext';
 
 const ExpenseItem = (props) => {
-    const { dispatch } = useContext(AppContext);
+    const { dispatch, currency } = useContext(AppContext);
 
     const handleDeleteExpense = () => {
         dispatch({
@@ -14,39 +14,33 @@ const ExpenseItem = (props) => {
         });
     };
 
-    const reduceAllocation = (name) => {
-        const expense = {
-            name: name,
-            cost: 10,
-        };
-
+    const reduceAllocation = () => {
         dispatch({
             type: 'RED_EXPENSE',
-            payload: expense
+            payload: {
+                name: props.name,
+                cost: 10,
+            },
         });
+    };
 
-    }
-
-    const increaseAllocation = (name) => {
-        const expense = {
-            name: name,
-            cost: 10,
-        };
-
+    const increaseAllocation = () => {
         dispatch({
             type: 'ADD_EXPENSE',
-            payload: expense
+            payload: {
+                name: props.name,
+                cost: 10,
+            },
         });
-
-    }
+    };
 
     return (
         <tr>
-        <td>{props.name}</td>
-        <td>£{props.cost}</td>
-        <td><FcPlus size='2.2em' onClick={event=> increaseAllocation(props.name)}></FcPlus></td>
-        <td><AiFillMinusCircle size='2.2em' color="red" onClick={event=> reduceAllocation(props.name)}></AiFillMinusCircle></td>
-        <td><TiDelete size='3em' color="red" onClick={handleDeleteExpense}></TiDelete></td>
+            <td>{props.name}</td>
+            <td>{currency}{props.cost}</td>
+            <td><FcPlus size='2.2em' onClick={increaseAllocation}></FcPlus></td>
+            <td><AiFillMinusCircle size='2.2em' color="red" onClick={reduceAllocation}></AiFillMinusCircle></td>
+            <td><TiDelete size='3em' color="red" onClick={handleDeleteExpense}></TiDelete></td>
         </tr>
     );
 };
